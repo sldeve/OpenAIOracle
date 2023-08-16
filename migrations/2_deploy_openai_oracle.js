beb9f2e6dede2e379e4029d42a6645dde1d5e31f
sliriano <sliriano2020@gmail.com>
2023-08-16 15:48:55 -0400

    fix truffle-config and compile/migrate

--- migrations/2_deploy_openai_oracle.js
+++ migrations/2_deploy_openai_oracle.js
@@ -2,7 +2,7 @@ const OpenAIOracle = artifacts.require("OpenAIOracle");
 
 module.exports = function(deployer, network, accounts) {
     // The oracle's account, for instance, can be the second account from Ganache
-    const oracleAccount = accounts[1];
+    const oracleAccount = accounts[0];
 
     deployer.deploy(OpenAIOracle, oracleAccount);
 };
